ca7ede8732923dc437671d000580785466953b90
murat <arkantarkan.33@gmail.com>
2021-09-09 11:54:35 +0100

    V4

--- src/projectvendingmachine/Products.java
+++ src/projectvendingmachine/Products.java
@@ -5,15 +5,13 @@ public class Products {
     private double snicker = 2.5;
     private double chipsAhoy = 2.2;
     private double pringles = 12.99;
-    private double milano =14.3;
+    private double milano = 14.3;
     private double reeses = 3.9;
     private double cocaCola = 2.2;
     private double fanta = 2.0;
     private double water = 1.0;
     private double arizona = 1.5;
     private double skittles = 1.99;
-
-
     private int productNum;
     private double price;
 
@@ -21,80 +19,40 @@ public class Products {
         return snicker;
     }
 
-    public void setSnicker(double snicker) {
-        this.snicker = snicker;
-    }
-
     public double getChipsAhoy() {
         return chipsAhoy;
-    }
-
-    public void setChipsAhoy(double chipsAhoy) {
-        this.chipsAhoy = chipsAhoy;
     }
 
     public double getPringles() {
         return pringles;
     }
 
-    public void setPringles(double pringles) {
-        this.pringles = pringles;
-    }
-
     public double getMilano() {
         return milano;
-    }
-
-    public void setMilano(double milano) {
-        this.milano = milano;
     }
 
     public double getReeses() {
         return reeses;
     }
 
-    public void setReeses(double reeses) {
-        this.reeses = reeses;
-    }
-
     public double getCocaCola() {
         return cocaCola;
-    }
-
-    public void setCocaCola(double cocaCola) {
-        this.cocaCola = cocaCola;
     }
 
     public double getFanta() {
         return fanta;
     }
 
-    public void setFanta(double fanta) {
-        this.fanta = fanta;
-    }
-
     public double getWater() {
         return water;
-    }
-
-    public void setWater(double water) {
-        this.water = water;
     }
 
     public double getArizona() {
         return arizona;
     }
 
-    public void setArizona(double arizona) {
-        this.arizona = arizona;
-    }
-
     public double getSkittles() {
         return skittles;
-    }
-
-    public void setSkittles(double skittles) {
-        this.skittles = skittles;
     }
 
     public int getProductNum() {
@@ -112,7 +70,6 @@ public class Products {
     public void setPrice(double price) {
         this.price = price;
     }
-
 
     @Override
     public String toString() {
--- src/projectvendingmachine/Runner.java
+++ src/projectvendingmachine/Runner.java
@@ -1,13 +1,17 @@
 package projectvendingmachine;
 
 public class Runner {
-
     public static void main(String[] args) {
+        System.out.println("Merhaba");
 
-        Products products = new Products();
+            Products product = new Products();
+            Method methods = new Method();
+            double money = methods.money();
+            System.out.println("Hesabinizda " + money + " dollar var");
+            methods.select(product);
+            methods.balance(product.getPrice(), money, product);
 
-        Method method =new Method();
-        method.select(products);
+
 
     }
 }
